061ed9031a28774c0ba3416c8d0a7a62d92d46ac
Sigfried Gold <sigfried@sigfried.org>
2019-05-16 08:54:38 -0400

    gh-pages only

---
+++ precache-manifest.d0a1bb39043fe1a6412bda6af01c68a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a73e17c2ac3dfbf3aca3",
+    "url": "/static/css/main.c44097cd.chunk.css"
+  },
+  {
+    "revision": "a73e17c2ac3dfbf3aca3",
+    "url": "/static/js/main.d791f7c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "554978fed4aab5b033f3",
+    "url": "/static/js/2.34d401e9.chunk.js"
+  },
+  {
+    "revision": "343f2f78b348055aefa79d5f77414b6d",
+    "url": "/static/media/daily.343f2f78.csv"
+  },
+  {
+    "revision": "884b35281828196c427db0805deb48b8",
+    "url": "/static/media/dcadaily.884b3528.csv"
+  },
+  {
+    "revision": "723db784f0c4d2e35dba9f0a70b95d37",
+    "url": "/index.html"
+  }
+];
